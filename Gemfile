780c816ecd5326bb062f9afef899c0a091f36d46
Philipp Hansch <dev@phansch.net>
2014-04-10 16:22:21 +0200

    Add streaming functionality

--- Gemfile
+++ Gemfile
@@ -1,11 +1,15 @@
 source 'https://rubygems.org'
 ruby File.read(".ruby-version").strip.split("-").first
-gem 'dotenv-rails', :groups => [:development, :test] # needs to be at top
 
 gem 'sinatra'
 gem 'pg'
-gem 'unicorn'
+gem 'thin'
 
 group :test, :development do
   gem 'pry-debugger'
+  gem 'dotenv-rails'
+end
+
+group :development do
+  gem 'rerun'
 end
--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
+require 'json'
 require 'sinatra'
 require 'dotenv'
 Dotenv.load
 
 # require stuff from stdlib
 require 'fileutils'
+
+
+set :server, %w[thin]
 
 helpers do
   def protected!
@@ -18,18 +22,46 @@ helpers do
   end
 end
 
+def timestamp
+  Time.now.strftime("%H:%M:%S")
+end
+connections = []
+notifications = []
+
 get '/' do
-  "Everybody can see this page"
+  @images = Dir["public/screenshots/*"].sort_by! {|filename| File.mtime(filename) }.map! do |image|
+    image[/screenshots\/\w*.((png)|(jpg))/]
+  end
+  erb :index
 end
 
 post '/screenshot/:filename' do
   protected!
-  dir = File.join("files", "screenshots")
-  FileUtils.mkdir_p(userdir)
+  dir = File.join("public", "screenshots")
+  FileUtils.mkdir_p(dir)
   filename = File.join(dir, params[:filename])
   datafile = params[:data]
   File.open(filename, 'wb') do |file|
     file.write(datafile[:tempfile].read)
   end
+  notification = params.merge( {'timestamp' => timestamp}).to_json
+
+  notifications << notification
+
+  notifications.shift if notifications.length > 10
+  connections.each { |out| out << "data: #{notification}\n\n"}
   "wrote to #{filename}\n"
 end
+
+get '/connect', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    connections << out
+
+    #out.callback on stream close evt. 
+    out.callback {
+      #delete the connection 
+      connections.delete(out)
+    }
+  end
+end
+
