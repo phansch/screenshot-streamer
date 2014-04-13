280327b2fd5ff335499c6f9eb1b7301a6d652527
Philipp Hansch <dev@phansch.net>
2014-04-13 15:57:55 +0200

    Add tests for adding the second image

--- spec/javascripts/MoveImageSpec.js
+++ spec/javascripts/MoveImageSpec.js
@@ -1,11 +1,37 @@
 var fixture;
 
 describe("MoveImages", function() {
-
   it('should have a fixture', function() {
     loadFixtures('oneImage.html');
     fixture = $('#test');
-    fixture.moveImages("whatever2.png");
     expect(fixture).toBeDefined();
+    fixture.remove();
+  });
+
+  describe("adding a second image", function() {
+    var fixture;
+
+    beforeEach(function() {
+      loadFixtures('oneImage.html');
+      fixture = $('#test');
+      fixture.moveImages("whatever2.png");
+    });
+
+    afterEach(function() {
+      fixture.remove();
+    });
+
+    it('should move the first image to the thumbnails', function() {
+      expect(fixture).toContainElement($('#thumbnails ul li'));
+    });
+
+    it('should replace the top image link with the new image path', function() {
+      expect('a#newest_image').toHaveAttr('href', '/screenshots/whatever2.png');
+    });
+
+    it('should replace the top image with the new image', function() {
+      expect('a#newest_image img').toHaveAttr('src', '/screenshots/whatever2.png');
+    });
+
   });
 });
